a33abc1e6b0e63618ab1ab4f79aea521cdcc4d26
albertovito <alberto.vito@outlook.it>
2024-12-25 17:55:49 +0100

    Moved to Walk

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module comparatore_sottotitoli
+module SrtComparator
 
 go 1.23.4
 
@@ -8,8 +8,11 @@ require (
 	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
 	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/image v0.20.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,92 +1,139 @@
 package main
 
 import (
-	"comparatore_sottotitoli/dialog"
-	"comparatore_sottotitoli/srt"
+	"SrtComparator/dialog"
+	"SrtComparator/srt"
 	"encoding/csv"
 	"fmt"
 	"os"
 
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 	"github.com/ncruces/zenity"
 )
 
 func main() {
-
 	// Leggi i sottotitoli da due file SRT
 	res := dialog.MainPage()
 	if !res {
 		return
 	}
-	file1, file2 := dialog.SecondPage()
+	// file1, file2 := dialog.SecondPage()
+	startWindow()
 
-	subtitles1, err := srt.ReadSRTFile(file1)
-	if err != nil {
-		fmt.Println("Errore nel leggere il primo file SRT:", err)
-		return
-	}
-	subtitles2, err := srt.ReadSRTFile(file2)
-	if err != nil {
-		fmt.Println("Errore nel leggere il secondo file SRT:", err)
-		return
-	}
+}
 
-	// Crea il file CSV
-	csvFile, err := os.Create("sottotitoli.csv")
-	if err != nil {
-		fmt.Println("Errore nella creazione del file CSV:", err)
-		return
-	}
-	defer csvFile.Close()
+func startWindow() {
+	var outTE1, outTE2 *walk.TextEdit
+	var file1, file2 string
 
-	// Crea un writer CSV con separatore ';'
-	writer := csv.NewWriter(csvFile)
-	writer.Comma = ';'
+	MainWindow{
+		Title:   "SrtComparator",
+		MinSize: Size{Width: 600, Height: 400},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &outTE1, ReadOnly: true, VScroll: true, Font: Font{Family: "Calibri 14"}},
+					TextEdit{AssignTo: &outTE2, ReadOnly: true, VScroll: true, Font: Font{Family: "Calibri 14"}},
+				},
+			},
+			HSplitter{
+				Children: []Widget{
+					PushButton{
+						Text: "File1",
+						OnClicked: func() {
+							file1 = dialog.PrintDialog("")
+							text1, _ := os.ReadFile(file1)
+							outTE1.SetText(string(text1))
+						},
+					},
+					PushButton{
+						Text: "File2",
+						OnClicked: func() {
+							file2 = dialog.PrintDialog("")
+							text2, _ := os.ReadFile(file2)
+							outTE2.SetText(string(text2))
+						},
+					},
+				},
+			},
+			PushButton{
+				Text: "Compara",
+				OnClicked: func() {
+					subtitles1, err := srt.ReadSRTFile(file1)
+					if err != nil {
+						fmt.Println("Errore nel leggere il primo file SRT:", err)
+						return
+					}
+					subtitles2, err := srt.ReadSRTFile(file2)
+					if err != nil {
+						fmt.Println("Errore nel leggere il secondo file SRT:", err)
+						return
+					}
 
-	// Scrivi l'intestazione del CSV
-	err = writer.Write([]string{"FILE1", "FILE2"})
-	if err != nil {
-		fmt.Println("Errore nel scrivere l'intestazione CSV:", err)
-		return
-	}
+					// Crea il file CSV
+					csvFile, err := os.Create("sottotitoli.csv")
+					if err != nil {
+						fmt.Println("Errore nella creazione del file CSV:", err)
+						return
+					}
+					defer csvFile.Close()
 
-	// Scrivi i sottotitoli combinati nel CSV
-	maxLength := len(subtitles1)
-	if len(subtitles2) > maxLength {
-		maxLength = len(subtitles2)
-	}
+					// Crea un writer CSV con separatore ';'
+					writer := csv.NewWriter(csvFile)
+					writer.Comma = ';'
 
-	// Scrivi le righe del CSV
-	for i := 0; i < maxLength; i++ {
-		var subtitle1, subtitle2 srt.Subtitle
-		if i < len(subtitles1) {
-			subtitle1 = subtitles1[i]
-		}
-		if i < len(subtitles2) {
-			subtitle2 = subtitles2[i]
-		}
+					// Scrivi l'intestazione del CSV
+					err = writer.Write([]string{"FILE1", "FILE2"})
+					if err != nil {
+						fmt.Println("Errore nel scrivere l'intestazione CSV:", err)
+						return
+					}
 
-		// Crea una riga per il CSV con il testo del sottotitolo per ogni file
-		record := []string{
-			srt.FormatSubtitle(&subtitle1),
-			srt.FormatSubtitle(&subtitle2),
-		}
+					// Scrivi i sottotitoli combinati nel CSV
+					maxLength := len(subtitles1)
+					if len(subtitles2) > maxLength {
+						maxLength = len(subtitles2)
+					}
 
-		// Scrivi la riga nel file CSV
-		err = writer.Write(record)
-		if err != nil {
-			fmt.Println("Errore nel scrivere una riga nel CSV:", err)
-			return
-		}
-	}
+					// Scrivi le righe del CSV
+					for i := 0; i < maxLength; i++ {
+						var subtitle1, subtitle2 srt.Subtitle
+						if i < len(subtitles1) {
+							subtitle1 = subtitles1[i]
+						}
+						if i < len(subtitles2) {
+							subtitle2 = subtitles2[i]
+						}
 
-	// Salva tutte le righe nel CSV
-	writer.Flush()
+						// Crea una riga per il CSV con il testo del sottotitolo per ogni file
+						record := []string{
+							srt.FormatSubtitle(&subtitle1),
+							srt.FormatSubtitle(&subtitle2),
+						}
 
-	// Verifica se ci sono errori nel flush
-	if err := writer.Error(); err != nil {
-		fmt.Println("Errore nel flush del file CSV:", err)
-		return
-	}
+						// Scrivi la riga nel file CSV
+						err = writer.Write(record)
+						if err != nil {
+							fmt.Println("Errore nel scrivere una riga nel CSV:", err)
+							return
+						}
+					}
 
-	zenity.Info("File generato correttamente :)", zenity.Title("Comparatore SRT"), zenity.NoIcon)
+					// Salva tutte le righe nel CSV
+					writer.Flush()
+
+					// Verifica se ci sono errori nel flush
+					if err := writer.Error(); err != nil {
+						fmt.Println("Errore nel flush del file CSV:", err)
+						return
+					}
+
+					zenity.Info("File generato correttamente :)", zenity.Title("Comparatore SRT"), zenity.NoIcon)
+					os.Exit(0)
+				},
+			},
+		},
+	}.Run()
 }
