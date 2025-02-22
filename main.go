5cdb39738b5283ed605964665b7481fa9b0b154e
albertovito <alberto.vito@outlook.it>
2025-02-22 16:02:48 +0100

    added columns tile

--- main.go
+++ main.go
@@ -31,11 +31,13 @@ func main() {
 		MinSize: declarative.Size{Width: 600, Height: 400},
 		Layout:  declarative.VBox{},
 		Children: []declarative.Widget{
+			// Area principale dove verranno stamparti i contenuti dei due file man mano che vengono caricati
 			declarative.HSplitter{
 				Children: []declarative.Widget{
 					declarative.VSplitter{
 						Children: []declarative.Widget{
 							declarative.TextEdit{AssignTo: &outTE1, ReadOnly: true, VScroll: true, Font: declarative.Font{Family: FONT}, Background: declarative.SolidColorBrush{Color: walk.RGB(255, 255, 255)}},
+							// Bottone per selezionare il file2
 							declarative.PushButton{
 								Text: "File1",
 								Font: declarative.Font{Family: FONT},
@@ -50,6 +52,7 @@ func main() {
 					declarative.VSplitter{
 						Children: []declarative.Widget{
 							declarative.TextEdit{AssignTo: &outTE2, ReadOnly: true, VScroll: true, Font: declarative.Font{Family: FONT}, Background: declarative.SolidColorBrush{Color: walk.RGB(255, 255, 255)}},
+							// Bottone per selezionare il file2
 							declarative.PushButton{
 								Text: "File2",
 								Font: declarative.Font{Family: FONT},
@@ -63,6 +66,8 @@ func main() {
 					},
 				},
 			},
+
+			// Bottone di conferma
 			declarative.PushButton{
 				Text: "Compara",
 				Font: declarative.Font{Family: FONT},
@@ -113,6 +118,11 @@ func generate(file1, file2 string) {
 		maxLength = len(subtitles2)
 	}
 
+	// Scrivo la prima riga della tabella
+	file.SetCellValue(sheet, "A1", "Index")
+	file.SetCellValue(sheet, "B1", "Timing")
+	file.SetCellValue(sheet, "C1", "File1")
+	file.SetCellValue(sheet, "D1", "File2")
 	// Scrivi le righe della tabella
 	for i := 0; i < maxLength; i++ {
 		var subtitle1, subtitle2 srt.Subtitle
@@ -122,11 +132,10 @@ func generate(file1, file2 string) {
 		if i < len(subtitles2) {
 			subtitle2 = subtitles2[i]
 		}
-
-		a := "A" + fmt.Sprint(i+1)
-		b := "B" + fmt.Sprint(i+1)
-		c := "C" + fmt.Sprint(i+1)
-		d := "D" + fmt.Sprint(i+1)
+		a := "A" + fmt.Sprint(i+2)
+		b := "B" + fmt.Sprint(i+2)
+		c := "C" + fmt.Sprint(i+2)
+		d := "D" + fmt.Sprint(i+2)
 		file.SetCellValue(sheet, a, subtitle1.Index)
 		file.SetCellValue(sheet, b, subtitle1.Start+" --->\n "+subtitle1.End)
 		file.SetCellValue(sheet, c, subtitle1.Text)
